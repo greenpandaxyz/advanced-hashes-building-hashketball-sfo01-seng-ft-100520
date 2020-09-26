61b9b188aabba4562dcbe3b07e6fba2a9215e215
Amanda D <amanda@depaula.xyz>
2020-09-26 17:21:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -86,7 +86,18 @@ def game_hash
         :assists => 7,
         :steals => 22,
         :blocks => 15,
-        :slam_dunks => 10,  
+        :slam_dunks => 10,
+      },
+      {
+        :player_name => "DeSagna Diop",
+        :number => 2,
+        :shoe => 14,
+        :points => 24,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 4,
+        :blocks => 5,
+        :slam_dunks => 5,  
       }
     ],
   } }

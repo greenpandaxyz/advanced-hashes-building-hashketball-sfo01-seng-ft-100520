f577224201b2694f75600d24a5651b6ec06ebfa1
Amanda D <amanda@depaula.xyz>
2020-09-26 17:05:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,10 +1,12 @@
 require 'pry'
+gem 'rufo'
 
 def game_hash
 {  home: {
 :team_name => "Brooklyn Nets",
 :colors => ["Black","White"],
-:players => [{
+:players => [
+  {
 :player_name =>"Alan Anderson",
   :number => 0,
   :shoe => 16,

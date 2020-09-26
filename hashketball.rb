972aa8d3dcc5be8a4625de66b5866308d60965db
Amanda D <amanda@depaula.xyz>
2020-09-26 16:25:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,14 @@
 def game_hash
-  
+  hash = [[:home => {
+    {:team_name =>}
+    {:colors =>}
+    {:players =>}
+  }]
+    [:away => {
+    {:team_name =>}
+    {:colors =>}
+    {:players =>}
+    }]
+
+  ]
 end

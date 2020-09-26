ba42e3f70d8deaaad23abd56c0491dd548477f50
Amanda D <amanda@depaula.xyz>
2020-09-26 17:02:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,22 +4,22 @@ def game_hash
 {  home: {
 :team_name => "Brooklyn Nets",
 :colors => ["Black","White"],
-:players => [{}]
-# :player_name =>"Alan Anderson",
-  # :number => 0,
-  # :shoe => 16,
-  # :points => 22,
-  # :rebounds => 12,
-  # :assists => 12,
-  # :steals => 3,
-  # :blocks => 1,
-  # :slam_dunks => 1}
-  # ]
-  },
-:away => {
-:team_name => "",
-:colors => [],
-:players => [{}]
-  }
+:players => [{
+:player_name =>"Alan Anderson",
+  :number => 0,
+  :shoe => 16,
+  :points => 22,
+  :rebounds => 12,
+  :assists => 12,
+  :steals => 3,
+  :blocks => 1,
+  :slam_dunks => 1
+  }]
+},
+# :away => {
+# :team_name => "",
+# :colors => [],
+# :players => [{}]
+#   }
 }
 end
